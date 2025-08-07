540b3c18b1d7a3334aca19846f44ff49117ba0d6
Yashvi-983 <chitrodayashvi@gmail.com>
2025-08-07 17:51:51 +0530

    modified files

--- Day-5-Task/callback.js
+++ Day-5-Task/callback.js
@@ -3,10 +3,18 @@
 function perform(c){
     console.log('Performing callback')
     c()
-}
+}     
 
 function callbackfunction(){
     console.log('Executing callback...')
 }
-
+                       
 perform(callbackfunction)
+
+
+function add(a, callback){      
+    let result = a + 4
+    callback(result)
+}
+
+add(2, sum => console.log(sum))
